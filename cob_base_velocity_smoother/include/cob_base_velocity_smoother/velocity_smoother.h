ad2eec7f9f83fe726cea0667ab7ab5f4154b2176
ipa-fxm <felix.messmer@ipa.fhg.de>
2017-02-11 18:07:06 +0100

    rework velocity_smoother

--- cob_base_velocity_smoother/include/cob_base_velocity_smoother/velocity_smoother.h
+++ cob_base_velocity_smoother/include/cob_base_velocity_smoother/velocity_smoother.h
@@ -1,6 +1,4 @@
 /**
- * @file /include/yocs_velocity_smoother/velocity_smoother_nodelet.hpp
- *
  * @brief Velocity smoother interface
  *
  * License: BSD
@@ -11,6 +9,7 @@
  *****************************************************************************/
 
  // Modified by Benjamin Maidel: benjamin.maidel@ipa.fraunhofer.de
+ //         and Felix Messmer:   felix.messmer@ipa.fraunhofer.de            
 
 #ifndef VELOCITY_SMOOTHER_HPP_
 #define VELOCITY_SMOOTHER_HPP_
@@ -23,6 +22,8 @@
 #include <dynamic_reconfigure/server.h>
 #include <cob_base_velocity_smoother/paramsConfig.h>
 #include <nav_msgs/Odometry.h>
+
+#include <boost/thread/mutex.hpp>
 
 /*****************************************************************************
 ** Namespaces
@@ -46,44 +47,34 @@ public:
   }
 
   bool init(ros::NodeHandle& nh);
-  void spin();
-  void shutdown() { shutdown_req = true; };
+  void spin(const ros::TimerEvent&);
 
 private:
-  enum RobotFeedbackType
-  {
-    NONE,
-    ODOMETRY,
-    COMMANDS
-  } robot_feedback;  /**< What source to use as robot velocity feedback */
-
   std::string name;
-  double speed_lim_vx, accel_lim_vx, decel_lim_vx, decel_lim_vx_safe;
-  double speed_lim_vy, accel_lim_vy, decel_lim_vy, decel_lim_vy_safe;
-  double speed_lim_w, accel_lim_w, decel_lim_w, decel_lim_w_safe;
-  double decel_factor, decel_factor_safe;
+  double speed_lim_vx, accel_lim_vx, decel_lim_vx;
+  double speed_lim_vy, accel_lim_vy, decel_lim_vy;
+  double speed_lim_w, accel_lim_w, decel_lim_w;
+  double decel_factor, safe_factor;
 
   double frequency;
 
   geometry_msgs::Twist last_cmd_vel;
-  geometry_msgs::Twist  current_vel;
-  geometry_msgs::Twist   target_vel;
+  geometry_msgs::Twist target_vel;
 
-  bool                 shutdown_req; /**< Shutdown requested by nodelet; kill worker thread */
   bool                 input_active;
-  double                cb_avg_time;
+  double               cb_avg_time;
   ros::Time            last_cb_time;
-  std::vector<double> period_record; /**< Historic of latest periods between velocity commands */
-  unsigned int             pr_next; /**< Next position to fill in the periods record buffer */
+  std::vector<double>  period_record; /**< Historic of latest periods between velocity commands */
+  unsigned int         pr_next;       /**< Next position to fill in the periods record buffer */
 
-  ros::Subscriber odometry_sub;    /**< Current velocity from odometry */
-  ros::Subscriber current_vel_sub; /**< Current velocity from commands sent to the robot, not necessarily by this node */
+  ros::Timer timer;
+
   ros::Subscriber raw_in_vel_sub;  /**< Incoming raw velocity commands */
   ros::Publisher  smooth_vel_pub;  /**< Outgoing smoothed velocity commands */
+  
+  boost::mutex _mutex;
 
   void velocityCB(const geometry_msgs::Twist::ConstPtr& msg);
-  void robotVelCB(const geometry_msgs::Twist::ConstPtr& msg);
-  void odometryCB(const nav_msgs::Odometry::ConstPtr& msg);
 
   double sign(double x)  { return x < 0.0 ? -1.0 : +1.0; };
 
@@ -92,6 +83,8 @@ private:
     nth_element(values.begin(), values.begin() + values.size()/2, values.end());
     return values[values.size()/2];
   };
+
+  double calculate_incs_per_sec(double target, double last_cmd, double acc_lim);
 
   /*********************
   ** Dynamic Reconfigure
--- cob_base_velocity_smoother/src/velocity_smoother_node.cpp
+++ cob_base_velocity_smoother/src/velocity_smoother_node.cpp
@@ -18,28 +18,16 @@ int main(int argc, char** argv)
   std::string name = unresolvedName(resolved_name); // unresolve it ourselves
 
   boost::shared_ptr<VelocitySmoother> vel_smoother_;
-  boost::shared_ptr<boost::thread>   worker_thread_;
 
   vel_smoother_.reset(new VelocitySmoother(name));
-  if (vel_smoother_->init(nh_p))
-  {
-    ROS_DEBUG_STREAM("Velocity Smoother: initialised [" << name << "]");
-    worker_thread_.reset(new boost::thread(&VelocitySmoother::spin, vel_smoother_));
-  }
-  else
+  if (!vel_smoother_->init(nh_p))
   {
     ROS_ERROR_STREAM("Velocity Smoother: initialisation failed [" << name << "]");
+    return -1;
   }
 
-  ros::Rate r(100);
-  while (ros::ok())
-  {
-    ros::spinOnce();
-    r.sleep();
-  }
-
-  vel_smoother_->shutdown();
-  worker_thread_->join();
+  ROS_DEBUG_STREAM("Velocity Smoother: initialised [" << name << "]");
+  ros::spin();
   
   return 0;
 }
